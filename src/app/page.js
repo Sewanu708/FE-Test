e94377681b22bf1c51696c8660b4441941872de7
Sewanu Isaiah <sewanu708@gmail.com>
2025-06-23 17:50:11 +0100

    feat: add global styles and layout for Next.js app
    - Introduced global CSS styles with Tailwind and custom properties for theming.
    - Created a RootLayout component to manage font imports and metadata.
    - Implemented a basic homepage with a centered heading.
    - Added utility functions for class name management using clsx and tailwind-merge.
    - Initialized a button component file for future development.

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-white text-black">
+       
+
+    </main>);
+}
---
+++ src/components/button.js
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+function button() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default button
