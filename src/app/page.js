65f9b420706edb90028ce85be1384a8d0c06b161
Sewanu Isaiah <sewanu708@gmail.com>
2025-06-23 17:52:36 +0100

    feat: implement button component and update Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import button from "@/components/button";
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-white text-black">
-       
+       {/* <button/> */}
 
     </main>);
 }
--- src/components/button.js
+++ src/components/button.js
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button"
 function button() {
   return (
     <>
+    <Button variant="outline">Button</Button>
     </>
   )
 }
